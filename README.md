48fed188fb96aa6ea7f6353d73c93d5c2100fad4
marinapapa <marina.vp@outlook.com>
2025-06-23 12:56:27 +0200

    run script example

--- README.md
+++ README.md
@@ -4,18 +4,25 @@
 ## Description 
 
 This repository proposes a structure to facilitate the use of the [rDaNCES](https://github.com/marinapapa/rDaNCES) package to run agent-based simulations based on the [DaNCES framework](https://github.com/marinapapa/DaNCES_framework). Specifically, we recommend setting up:
+
 1. A 'code' folder to include the scripts that run simulations and analyze the simulated data. See __get_started.R__ script for a step by step guide.
 
 2. The rDaNCES folder (currently included as a submodule).
 
-3. A 'simulation' folder that includes the model executables to be used. For a trial in Windows, the user can insert the compiled Release found in the DaNCES_framework repo. To keep track in the README of each full model, the user is advised to take advantage of version control of their model's GitHub repository, for instance:
+3. A 'simulator' folder that includes the model executables (preferably the full bin folder) to be used (see example of *sim_demo*). For a trial in Windows, the user can insert the compiled Release found in the DaNCES_framework repo. To keep track in the README of each full model, the user is advised to take advantage of version control of their model's GitHub repository, for instance:
 - demo_model.exe: branch *develop* @583ebdf (tree: 732qsfda4n, commit am: 'exp 1 model')
 - demo_model_v2.exe: branch *develop* @535edde (tree: 732qsfda4n, commit am: 'exp 2 model')
 
+## Output
+
+The model will create a sim_data folder in the repository where it will store all simulated data in a subdirectory named as in the default config (here 'test') or set through the experiment set up (here in get_started.R set as 'rdances_demo').
+
 ## Prerequisites
-Apart from the R dependencies included in the package's documentation, to run the agent-based simulations one also needs to:
-* Define enviornmental variable *dancesPATH* with the path to the full *dances* repo
+Apart from the R dependencies included in the package's documentation, to run the agent-based simulations:
 * See prerequesites of C++ model [DaNCES](https://github.com/marinapapa/DaNCES_framework)
+
+In Windows, using a precompiled model, you may also need to:
+* Define an enviornmental variable for the path to the full *dances* repo
 
 ## Publication
 
--- code/get_started.R
+++ code/get_started.R
@@ -13,11 +13,12 @@
 ## bin folder of a Release: https://github.com/marinapapa/DaNCES_framework/releases
 
 ####################################################
-## Step 2. Clone the submodule repository of rDaNCES 
-## here 
+## Step 2. Make sure the submodule repository of rDaNCES is 
+## also in the main repository folder.
 
-## Build the rDaNCES package
-devtools::build("rDaNCES")
+## Install the rDaNCES package from source. Alternative you can use the 
+## devtools package to install it from github.
+install.packages("../rDaNCES/", repos = NULL, type="source")
 
 ###########################
 ## Step 3. Set repositories
@@ -26,13 +27,13 @@ devtools::build("rDaNCES")
 exp_name <- 'rdances_demo'
 
 # Relative path to the model's executable
-model_exe_path <- '..\\Simulation\\sim_demo\\dances.exe'
+model_exe_path <- 'simulator\\sim_demo\\bin\\dances.exe'
 
 # Use the subdirectory that the .exe is in as the main simulation repository
-main_repo <- dirname(model_exe_path)
+main_repo <- 'simulator\\sim_demo'
 
 # The default configuration file to use (should  be in the folder)
-config_tmp_name <- paste0(main_repo, '/demo_config.json')
+config_tmp_name <- paste0(main_repo, '\\demo_config.json')
 
 # Create folders' structure
 dirs <- rDaNCES::set_up_directories(sim_name = exp_name,
@@ -40,12 +41,16 @@ dirs <- rDaNCES::set_up_directories(sim_name = exp_name,
                            model_exe = model_exe_path,
                            main_path =  main_repo
 )
+## New folders should be created to host the generated configs and
+## parameter space tracking.
+
 
 ##################################
-## Step 4. Check model sun
+## Step 4. Check model run
 ## Try to run the model with the default config.
 ## The results should be stored in a sim_data folder.
 
+## Interactive mode with GUI:
 shell(paste0(dirs$MODEL_RUN_DIR,
              ' config=', 
              config_tmp_name  
@@ -57,7 +62,7 @@ shell(paste0(dirs$MODEL_RUN_DIR,
 ## Create configuration files across the parameter space
 
 ### Parameter values
-N <- c(10, 50, 100, 500, 1000) # group size
+N <- c(10, 20, 50) # group size
 
 # Number of repetitions of each parameter set:
 reps <- 2
@@ -79,3 +84,37 @@ print(paste0('We will run ',
 ## Run all simulations
 
 rDaNCES::run_simulations(dirs, reps)
+
+
+#############################################
+## Step 7. Load the data from the simulations
+## Store them in a list of dataframes.
+
+## The data are stored in a sim_data folder in 
+## the main repository, in a folder named after
+## the experiment. 
+
+sim_data <- rDaNCES::import_base_data('sim_data/rdances_demo/20256231235481750674948780/',
+                                      types = c('self'))
+
+head(sim_data$self) ## the individual timeseries 
+## the other elements of the list are the config
+## and a simulation information list:
+head(sim_data$sim_info)
+
+
+## We can then import all the data from the simulations we run:
+sim_data <- rDaNCES::import_multi_data('sim_data/rdances_demo/',
+                                      types = c('self'))
+
+## Now each element of the list is a simulation list as before
+head(sim_data[[1]]$self) 
+
+## To merge together results with the same configuration (parameter file)
+
+sim_data <- rDaNCES::accum_sim_data(sim_data)
+
+## Now each element of the list is a set with repetitions, with the 
+## individual simulation ids (and how many runs) included in the 
+## information file:
+sim_data[[1]]$sim_info
